0b160c84bbc34276ca2e371300c0c992301971a1
luciallz <lucialleras.zarza@gmail.com>
2022-11-01 10:45:57 +0100

    menu bien alineado

--- herfontsistemas-app/src/App.js
+++ herfontsistemas-app/src/App.js
@@ -5,7 +5,6 @@ import Form from './componentes/Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './componentes/img/logo.jpg';
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import EmojiPicker from 'emoji-picker-react';
 import { Nav, Navbar, NavbarBrand, NavDropdown } from 'react-bootstrap'
 function App() {
   const [usuarios, setUsuario]=useState([])
@@ -64,37 +63,42 @@ function App() {
         <div className='col'>
           <h1>HERFONTSISTEMAS WEB</h1>
           <Navbar bg="green" variant="dark" sticky='top' expand='lg'>
-            <Navbar.Brand>
-              <img src={logo} height={'85px'} width={'205px'}></img>
-            </Navbar.Brand>
+            <NavbarBrand>
+              <img src={logo} height='85px' width='205px'></img>
+            </NavbarBrand>
             
             <Navbar.Toggle/>
               <Navbar.Collapse>
-              <Nav>
-              <NavDropdown title="Productos">
-                <NavDropdown.Item href="productos/abrazaderas">Abrazaderas de tuberia</NavDropdown.Item>
-                <NavDropdown.Item href="productos/abrazaderas">Conectores SML</NavDropdown.Item>
-                <NavDropdown.Item href="productos/abrazaderas">Accesorios de montaje</NavDropdown.Item>
-                <NavDropdown.Item href="productos/abrazaderas">Sistemas de montaje para ventilación</NavDropdown.Item>
-                <NavDropdown.Item href="productos/abrazaderas">CENTUM</NavDropdown.Item>
-                <NavDropdown.Item href="productos/abrazaderas">Carriles de perfil</NavDropdown.Item>
-                <NavDropdown.Item href="productos/abrazaderas">Aisladores de resorte</NavDropdown.Item>
-                <NavDropdown.Item href="productos/abrazaderas">Abrazaderas de tubería aisladas</NavDropdown.Item>
-                <NavDropdown.Item href="productos/abrazaderas">Accesorios de trabajo, heramientas...</NavDropdown.Item>
-                <NavDropdown.Item href="productos/abrazaderas">Sistemas de montaje para rociadortes</NavDropdown.Item>
-                <NavDropdown.Item href="productos/abrazaderas">Puntos fijos</NavDropdown.Item>
-                <NavDropdown.Item href="productos/abrazaderas">Elementos deslizantes</NavDropdown.Item>
-                <NavDropdown.Item href="productos/abrazaderas">Recubrumiento TSP-3</NavDropdown.Item>
-                <NavDropdown.Item href="productos/abrazaderas">Sistemas de montaje acero inoxidable</NavDropdown.Item>
-              </NavDropdown>
+                  <Nav>
+                    <NavDropdown title="Productos">
+                      <NavDropdown.Item href="productos/abrazaderas">Abrazaderas de tuberia</NavDropdown.Item>
+                      <NavDropdown.Item href="productos/abrazaderas">Conectores SML</NavDropdown.Item>
+                      <NavDropdown.Item href="productos/abrazaderas">Accesorios de montaje</NavDropdown.Item>
+                      <NavDropdown.Item href="productos/abrazaderas">Sistemas de montaje para ventilación</NavDropdown.Item>
+                      <NavDropdown.Item href="productos/abrazaderas">CENTUM</NavDropdown.Item>
+                      <NavDropdown.Item href="productos/abrazaderas">Carriles de perfil</NavDropdown.Item>
+                      <NavDropdown.Item href="productos/abrazaderas">Aisladores de resorte</NavDropdown.Item>
+                      <NavDropdown.Item href="productos/abrazaderas">Abrazaderas de tubería aisladas</NavDropdown.Item>
+                      <NavDropdown.Item href="productos/abrazaderas">Accesorios de trabajo, heramientas...</NavDropdown.Item>
+                      <NavDropdown.Item href="productos/abrazaderas">Sistemas de montaje para rociadortes</NavDropdown.Item>
+                      <NavDropdown.Item href="productos/abrazaderas">Puntos fijos</NavDropdown.Item>
+                      <NavDropdown.Item href="productos/abrazaderas">Elementos deslizantes</NavDropdown.Item>
+                      <NavDropdown.Item href="productos/abrazaderas">Recubrumiento TSP-3</NavDropdown.Item>
+                      <NavDropdown.Item href="productos/abrazaderas">Sistemas de montaje acero inoxidable</NavDropdown.Item>
+                    </NavDropdown>
 
-              <Nav.Link href="servicios">Servicios</Nav.Link>
-              <Nav.Link href="sobre nosotros">Sobre nosotros</Nav.Link>
-              <Nav.Link href="contacto">Contacto</Nav.Link>
-              <i className="bi bi-person fs-1 text-white text-right"></i>
-            </Nav>
-                
-            </Navbar.Collapse>
+                    <Nav.Link href="servicios">Servicios</Nav.Link>
+                    <Nav.Link href="sobre nosotros">Sobre nosotros</Nav.Link>
+                    <Nav.Link href="contacto">Contacto</Nav.Link>
+                    <Nav.Link href="usuarios">Usuarios</Nav.Link>{/* solo se ve en rol de administrador */}
+                    <Nav.Link href="trabajadores">Trabajadores</Nav.Link> {/* solo se ve en rol de administrador */}
+
+                  </Nav>
+              </Navbar.Collapse>
+              <i className="bi bi-person fs-1 text-white text-right"></i> {/* No puedo alinearlo a la derecha*/}
+              <i class="bi bi-cart3 fs-1 text-white text-right"></i> {/* No puedo alinearlo a la derecha*/}
+
+
           </Navbar>
           
         </div>
