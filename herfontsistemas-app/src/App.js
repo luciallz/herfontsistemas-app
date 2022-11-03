1714d21c3373e0dc869685cd9ddc17721691750c
luciallz <lucialleras.zarza@gmail.com>
2022-11-03 16:31:09 +0100

    subir

--- herfontsistemas-app/src/App.js
+++ herfontsistemas-app/src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import Menu from './componentes/Menu'
-import AppRoute from './rutas';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import Menu from './componentes/Menu1';
+import {BrowserRouter, Routes ,Route} from 'react-router-dom'
+import Registro from './pages/Registro';
 function App() {
 
   return (
-    <div className='container'>
-      <div className='row'>
-        <div className='col'>
-            <Menu />
-        </div>
-      </div>  
-    </div>
+    
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>
+          <Menu /> 
+          <Registro />
+            <BrowserRouter>
+              <Routes>
+                
+
+              </Routes>
+            </BrowserRouter>
+             
+          </div>
+        </div>  
+      </div>
   )
 }
 
