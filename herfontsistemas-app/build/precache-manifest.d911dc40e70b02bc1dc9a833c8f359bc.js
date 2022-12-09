00699d11f449c26bd579939d78ff73e21d7619ff
Lucía <82877049+luciallz@users.noreply.github.com>
2022-12-09 17:28:14 +0100

    build

---
+++ herfontsistemas-app/build/precache-manifest.d911dc40e70b02bc1dc9a833c8f359bc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "e202495c46ba5bb4f364",
+    "url": "/static/css/main.5815cee4.chunk.css"
+  },
+  {
+    "revision": "e202495c46ba5bb4f364",
+    "url": "/static/js/main.e202495c.chunk.js"
+  },
+  {
+    "revision": "d5fa0de6faffdf64f6d1",
+    "url": "/static/js/1.d5fa0de6.chunk.js"
+  },
+  {
+    "revision": "4a97de56a08415f81289",
+    "url": "/static/css/2.11e45a1f.chunk.css"
+  },
+  {
+    "revision": "4a97de56a08415f81289",
+    "url": "/static/js/2.4a97de56.chunk.js"
+  },
+  {
+    "revision": "072245c68243332ad8a1",
+    "url": "/static/js/runtime~main.072245c6.js"
+  },
+  {
+    "revision": "be6df3f0c9855796ae0c099ee4ca43ce",
+    "url": "/static/media/bootstrap-icons.be6df3f0.woff"
+  },
+  {
+    "revision": "31e1300d419245fd27614630601dc74d",
+    "url": "/static/media/bootstrap-icons.31e1300d.woff2"
+  },
+  {
+    "revision": "6a0e6cedb2fc3bf5332928247785a525",
+    "url": "/static/media/logo.6a0e6ced.jpg"
+  },
+  {
+    "revision": "c0929a1e055f2ceb5c889501c24a24e1",
+    "url": "/static/media/primeicons.c0929a1e.woff2"
+  },
+  {
+    "revision": "c08d00cac4f2edb5ade360a193fcd08e",
+    "url": "/static/media/primeicons.c08d00ca.eot"
+  },
+  {
+    "revision": "062070972fbad0f4fbb3505e6d013a39",
+    "url": "/static/media/primeicons.06207097.woff"
+  },
+  {
+    "revision": "acb19296cb26b15dcf42e13c603217f2",
+    "url": "/static/media/primeicons.acb19296.ttf"
+  },
+  {
+    "revision": "1d854e3ab2c37173804a0d1d1af229ee",
+    "url": "/static/media/primeicons.1d854e3a.svg"
+  },
+  {
+    "revision": "8e55aa374aa7ba66b39023b400b09ed7",
+    "url": "/index.html"
+  }
+];
