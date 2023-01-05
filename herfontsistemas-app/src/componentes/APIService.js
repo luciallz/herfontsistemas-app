600d17fe98cf8a842cce229aab26c704c61d79f6
luciallz <lucialleras.zarza@gmail.com>
2023-01-05 10:16:19 +0100

    cambiar rutas

--- herfontsistemas-app/src/componentes/APIService.js
+++ herfontsistemas-app/src/componentes/APIService.js
@@ -11,7 +11,7 @@ export default class APIService {
   }
 
   static InsertarUsuario(body) {
-    return fetch(rutaMaquina + `/nuevo`, {
+    return fetch(`/nuevo`, {
       'method': 'POST',
       headers: { "Content-type": "application/json" },
       body: JSON.stringify(body)
