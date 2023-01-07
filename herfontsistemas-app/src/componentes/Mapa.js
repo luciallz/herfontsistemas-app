f64f40ad10d65da1654e861ed8a1955f2216fe9f
melanyzuritaa <56831365+melanyzuritaa@users.noreply.github.com>
2023-01-07 11:49:46 +0100

    cambio mapa

--- herfontsistemas-app/src/componentes/Mapa.js
+++ herfontsistemas-app/src/componentes/Mapa.js
@@ -4,6 +4,6 @@ import 'leaflet/dist/leaflet.css'
 
 export const Mapa = () => {
     return (
-        <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-    )
+        <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=C/%20del%20Estanque,%205.%20P.I.%20La%20Fontana,%20nave%20n%C2%B0%203.%20%2028945%20Fuenlabrada%20Madrid&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+        )
 }
